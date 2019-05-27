e362513f86de7ec3f59706d6c108b02f15b062a0
Unknown <michal.m.zarach@gmail.com>
2019-05-27 11:56:24 +0200

    Switch to cloud amqp

--- notifications-service/src/config/index.js
+++ notifications-service/src/config/index.js
@@ -1,7 +1,7 @@
 
 
 module.exports = {
-    amqp_url: 'amqp://guest:guest@rabbitmq',
+    amqp_url: process.env.AMQP_URL || 'amqp://guest:guest@rabbitmq',
     //amqp_url: 'amqp://guest:guest@localhost',
 
     q: process.env.NOTIFICATIONS_QUEUE || 'notifications'
